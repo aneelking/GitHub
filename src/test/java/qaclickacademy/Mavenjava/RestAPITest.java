36f5f8f7fca111bc7a27c4e41b47a73db308399a
amala <amalathinnelapudi@gmail.com>
2021-01-08 17:57:15 +0530

    first messge

---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void PostJibra()
+	{
+		System.out.println("PostJibra");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
